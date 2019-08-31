883bf95a3853e2901abed2137f1e9326da2c2166
David Ingram <david@ingramaviation.com>
2019-08-31 22:23:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -79,7 +79,6 @@ def sell_pets
 end 
 
  def list_pets
-   #"I have #{Dog.all.size / 10} dog(s), and #{Cat.all.size / 10} cat(s)."
    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
  end 
 
